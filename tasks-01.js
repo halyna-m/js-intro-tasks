16a4c68d7e12eef3b0c31b04751ef2937463b24a
halyna-m <66943711+halyna-m@users.noreply.github.com>
2020-12-14 23:06:23 +0100

    Create tasks-01.js

---
+++ tasks-01.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  let messiGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  return messiGoals;
+}
