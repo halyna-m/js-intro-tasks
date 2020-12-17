99c331dda63a1cf5ad70d2b82f5c8f5acc7bf5ee
halyna-m <66943711+halyna-m@users.noreply.github.com>
2020-12-17 16:48:25 +0100

    Create tasks-02.js

---
+++ tasks-02.js
@@ -0,0 +1,32 @@
+// https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+
+function stringToArray(string) {
+  let array = string.split(" ");
+  return array;
+}
+
+
+// https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+function DNAtoRNA(dna) {
+  return dna.replace(/T/g, "U"); 
+}
+
+
+// https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+const min = function (list) {
+  return Math.min.apply(Math, list);
+};
+
+const max = function (list) {
+  return Math.max.apply(Math, list);
+};
+
+
+// https://www.codewars.com/kata/beginner-lost-without-a-map
+
+function maps(x) {
+  const newMap = x.map((x) => x * 2);
+  return newMap;
+}
