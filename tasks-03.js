303d7df32ab0383c7013f6ee5138e50e92dc2ac1
halyna-m <66943711+halyna-m@users.noreply.github.com>
2020-12-19 12:25:02 +0100

    Create tasks-03.js

---
+++ tasks-03.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args)
+  }
+}
+
+// https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+
