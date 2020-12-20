48e508473c8d1927c51f58b05ff6171078bc16c0
halyna-m <66943711+halyna-m@users.noreply.github.com>
2020-12-20 19:44:39 +0100

    Update tasks-03.js

--- tasks-03.js
+++ tasks-03.js
@@ -54,3 +54,28 @@ class Dog extends Animal {
     return `Hello ${this.master}`;
   }
 }
+
+// https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+
+const giveMeFive = (object) => {
+  const result = [];
+  for (const key in object) {
+    if (object.hasOwnProperty(key)) {
+      if (key.length === 5) result.push(key);
+      if (object[key].length === 5) result.push(object[key]);
+    }
+  }
+  return result;
+};
+
+// https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+
+function buildFun(n) {
+  let result = [];
+  for (let i = 0; i < n; i++) {
+    result[i] = function () {
+      return i;
+    };
+  }
+  return result;
+}
